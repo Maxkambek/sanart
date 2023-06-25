694c6128615eef4aafd2494887201388aa4e3c13
Maxkambek <mahkamkasimov94@gmail.com>
2023-06-25 13:53:21 +0500

    user balance

--- config/routing.py
+++ config/routing.py
@@ -1,11 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import auction.routing
+from channels.security.websocket import OriginValidator
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    'websocket': OriginValidator(
         URLRouter(
             auction.routing.websocket_urlpatterns
-        )
+        ),
+        ['*']
     ),
 })
